d14430b554a7779275cb9dec3765fb194d9573bf
JAMES FRANK MENDOZA RIOS <21100654@ue.edu.pe>
2026-06-08 23:54:57 -0500

    dsf

--- app/components/landing/bannerCallToAction.component.vue
+++ app/components/landing/bannerCallToAction.component.vue
@@ -7,7 +7,7 @@ const redirigirRegistro = () => {
 
 <template>
   <div class="cta-container">
-    <button class="cta-banner-button" @click="redirigirRegistro">
+    <button class="cta-banner-button" url="https://docs.google.com/forms/d/e/1FAIpQLSfXXXXXXXXX/viewform" >
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="icon-user-add">
         <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
       </svg>
